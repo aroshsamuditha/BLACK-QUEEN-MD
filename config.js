a0cb420bf317edf532f6508af049eb995f7e9a6d
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2024-08-11 09:05:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/vX31j64/BLACK-QUEEN.png",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hi,im alive...",
 };
