fdd511aad2c2fbc8dda7d6125eac8eb977e211b8
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2024-08-16 08:53:07 +0530

    Update voice.js

--- plugins/voice.js
+++ plugins/voice.js
@@ -1,6 +1,6 @@
 client.on('message', async msg => {
     if (msg.body === '.alive') {
-        const voiceMessage = fs.readFileSync('path/to/voice/file.ogg'); // Replace with the path to your voice file
+        const voiceMessage = fs.readFileSync('Media/alive.mp3'); // Replace with the path to your voice file
         await client.sendMessage(msg.from, voiceMessage, { sendAudioAsVoice: true });
     }
 });
