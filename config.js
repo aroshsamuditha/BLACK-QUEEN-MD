07a71daded50d49fd1eb1c94874f74301701cbca
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2024-08-16 16:00:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/vX31j64/BLACK-QUEEN.png",
-ALIVE_MSG: process.env.ALIVE_MSG || "*ඉන්නව ඉනවා තාම පණපිටින් 💭✅*\n\n*👸🏻BLACK QUEEN 🖤🪄*\n`create by Arosh Samuditha`\n\n```Contact Owner:-```\nhttps://wa.me/94761676948?text=*Hellow+Arosh+😁🩷*",
+ALIVE_MSG: process.env.ALIVE_MSG || "*ඉන්නව ඉන්නවා තාම පණපිටින් 💭✅*\n\n*👸🏻BLACK QUEEN 🖤🪄*\n`create by Arosh Samuditha`\n\n```Contact Owner:-```\nhttps://wa.me/94761676948?text=*Hellow+Arosh+😁🩷*",
 };
