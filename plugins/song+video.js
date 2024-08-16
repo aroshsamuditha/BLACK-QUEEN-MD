c963df45a21f77b69b6e9fb29ccd2eaf43d5893b
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2024-08-17 02:48:38 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -39,7 +39,7 @@ await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",f
   
 }catch(e){
   console.log(e)
-  reply('${e}')
+  reply(`${e}`)
 }
 })
 
@@ -84,6 +84,6 @@ await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fi
   
 }catch(e){
   console.log(e)
-  reply('${e}')
+  reply(`${e}`)
 }
 })
