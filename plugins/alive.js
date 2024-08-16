ca80277b9c071696f74d2095c69121a2f259a849
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2024-08-17 02:35:01 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,6 +10,8 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+return await conn.sendMessage(from,{voice: {url: "https://github.com/aroshsamuditha/BLACK-QUEEN-MD/blob/e9aa03e4658176b5ba99e6387f72ae3a31d89b78/Media/alive.mp3"},{quoted: mek})
+
 }catch(e){
 console.log(e)
 reply(`${e}`)
