54483b0144aefa87344e7b4d1fb7e94404326255
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2024-08-17 05:19:35 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -30,9 +30,11 @@ await conn.sendMessage(from,{image:{url:data.thumbnail},caption:desc},{quoted:me
 let down = await fg.yta(url)
 let downloadUrl = down.dl_url
 
-//send audio
+//send audio + doc
 
 await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"Arosh"},{quoted:mek})
+
 
 
   
@@ -41,3 +43,50 @@ await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{qu
   reply(`${e}`)
 }
 })
+
+
+//============video download============
+
+cmd({
+    pattern: "video",
+    desc: "download video",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("කැප්ශන් එක")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+let desc = `
+your bot
+
+title: ${data.title}
+description: ${data.description}
+time: ${data.time}
+ago: ${data.ago}
+views: ${data.views}
+  
+`
+await conn.sendMessage(from,{image:{url:data.thumbnail},caption:desc},{quoted:mek});
+//download video
+
+let down = await fg.ytv(url)
+let downloadUrl = down.dl_url
+
+//send video + doc
+
+await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"Arosh"},{quoted:mek})
+
+  
+}catch(e){
+  console.log(e)
+  reply(`${e}`)
+}
+})
+
+
+
