883a58e79fe488a351477f2fefcf908dab0f13d6
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2024-08-16 08:59:26 +0530

    Delete plugins/voice.js

--- plugins/voice.js
+++
@@ -1,9 +0,0 @@
-client.on('message', async msg => {
-    if (msg.body === '.alive') {
-        const voiceMessage = fs.readFileSync('Media/alive.mp3'); // Replace with the path to your voice file
-        await client.sendMessage(msg.from, voiceMessage, { sendAudioAsVoice: true });
-    }
-});
-
-// Initialize the client
-client.initialize();
