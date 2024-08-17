3bebdaf30e883451453ca9e859f6704f3deb4b48
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2024-08-17 20:43:53 +0530

    Create ping.js

---
+++ Commands/ping.js
@@ -0,0 +1,19 @@
+const { zokou } = require("../framework/zokou");
+const moment = require("moment-timezone");
+const { default: axios } = require('axios');
+const conf = require('../set');
+
+zokou({
+        pattern: "ping",
+        desc: "To check ping",
+        category: "test",
+        filename: __filename,
+    },
+    async(Void, citel) => {
+        var inital = new Date().getTime();
+        const { key } = await Void.sendMessage(citel.chat, {text: 'PING BLACK-QUEEN MD'});
+        var final = new Date().getTime();
+       // await Secktor.sleep(1000)
+       return await Void.sendMessage(citel.chat, {text: 'BLACK-QUEEN PING\n ' + (final - inital) + ' ms ', edit: key});
+    }
+);
