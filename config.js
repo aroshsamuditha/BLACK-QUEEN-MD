a4241493e56eaaad2175015a55c3ca06c491a60a
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2024-08-17 17:10:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/vX31j64/BLACK-QUEEN.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*ඉන්නව ඉන්නවා තාම පණපිටින් 💭✅*\n\n*👸🏻BLACK QUEEN 🖤🪄*\n`create by Arosh Samuditha`\n\n```Contact Owner:-```\nhttps://wa.me/94761676948?text=*Hellow+Arosh+😁🩷*\n\n🔖අපගේ අනෙකුත් Updates ගැන දැනගැනීමට පහත link වෙත පිවිසෙන්න 🔗\n\n*+ Cool Art Graphic🔗*\n> https://chat.whatsapp.com/FRsIjml10CWAX7NAPF7xIb\n*+ Black Queen Bot Group🔗*\n> https://chat.whatsapp.com/IT6mjqGINN6LaLSKnTZd6r\n*+ FaceBook🔗*\n> https://www.facebook.com/profile.php?id=61550302625124&mibextid=ZbWKwL\n\n*© ʙʟᴀᴄᴋ Qᴜᴇᴇɴ ʙʏ ᴀʀᴏꜱʜ ꜱᴀᴍᴜᴅɪᴛʜᴀ*",
-ALIVE_VOICE: process.env.ALIVE_VOICE || "https://github.com/aroshsamuditha/BLACK-QUEEN-MD/blob/4c4aabe9d380b3bcf858a270e30b9fe8e90ce615/Media/alive.mp3",
+ALIVE_VOICE: process.env.ALIVE_VOICE || "https://github.com/aroshsamuditha/BLACK-QUEEN-MD/raw/4c4aabe9d380b3bcf858a270e30b9fe8e90ce615/Media/alive.mp3",
     
 };
