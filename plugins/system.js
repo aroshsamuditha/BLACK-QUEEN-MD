95469c44d295436870efb84c550554e0643a3275
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2024-08-19 20:29:17 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,26 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+cmd({
+    pattern: "system",
+    alias:["status","botinfo","blackqueen"],
+    desc: "Check up time,ram usage and more.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let status = `*⏱️ Uptime:*  ${runtime(process.uptime())}
+*📟 Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*🧠 HostName:* ${os.hostname()}
+*🤖 Owner:* Arosh Samuditha
+
+*© ʙʟᴀᴄᴋ Qᴜᴇᴇɴ ʙʏ ᴀʀᴏꜱʜ ꜱᴀᴍᴜᴅɪᴛʜᴀ*
+`
+ return reply(`${status}`)
+}catch(e){
+console.log(e)
+  reply(`${e}`)
+
+}
+})
