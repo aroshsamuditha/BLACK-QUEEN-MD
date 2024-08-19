dbb582d80ce89e6073461fa91ac9e9284c11cafd
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2024-08-20 00:24:08 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -4,7 +4,7 @@ const os = require("os")
 const {runtime} = require('../lib/functions')
 cmd({
     pattern: "system",
-    alias:["status","botinfo","blackqueen"],
+    alias:["botinfo","blackqueen"],
     desc: "Check up time,ram usage and more.",
     category: "main",
     filename: __filename
