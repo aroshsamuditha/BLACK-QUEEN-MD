be0657c1fcb86a7dcadb6ea9a6e200ef6bd4a090
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2024-08-26 08:21:28 +0530

    Update voicemsg.js

--- plugins/voicemsg.js
+++ plugins/voicemsg.js
@@ -11,7 +11,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
     try {
         
         // Send voice message
-        await conn.sendMessage(from, { audio: { url: config.ALIVE_AUDIO }, mimetype: 'audio/mp4' }, { quoted: mek });
+        await conn.sendMessage(from, { audio: { url: config.ALIVE_AUDIO }, mimetype: 'audio/mp3' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`${e}`);
@@ -27,7 +27,7 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         // Send voice message for menu
-        await conn.sendMessage(from, { audio: { url: config.MENU_AUDIO }, mimetype: 'audio/mp4' }, { quoted: mek });
+        await conn.sendMessage(from, { audio: { url: config.MENU_AUDIO }, mimetype: 'audio/mp3' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`${e}`);
