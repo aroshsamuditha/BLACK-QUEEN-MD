9559d8dc3a517d0e438d0bbd1044659216b47f9d
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2024-08-26 08:33:40 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,7 +10,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
-    await conn.sendMessage(from, { audio: { url: 'Media/alive.MP3' }, mimetype: 'audio/mp4' }, { quoted: mek });
+  return  await conn.sendMessage(from, { audio: { url: 'Media/alive.MP3' }, mimetype: 'audio/mp4' }, { quoted: mek });
 //return await conn.sendMessage(from,{audio: {url: config.ALIVE_AUDIO},{quoted: mek})
 
 }catch(e){
