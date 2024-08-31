ab4b8ba0c225006e9076c4018dda1e776cacf1bd
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2024-08-31 18:07:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const prefix = '.'
 
 const ownerNumber = ['9471676948']
 
@@ -43,9 +42,12 @@ async function connectToWA() {
 //connect mongodb ================================
       const connectDB = require('./lib/mongodb')
        connectDB();
- //==================================================   
+ //==================================================  
 const {readEnv} = require('./lib/database')
 const config = await readEnv();
+const prefix = config.PREFIX
+//=================================================
+      
 console.log("Connecting BLACK QUEEN 🖤👸...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
